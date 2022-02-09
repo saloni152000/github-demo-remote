62fac72ecff27241c565c754f206ef074fab06c8
saloni152000 <70310137+saloni152000@users.noreply.github.com>
2022-02-09 16:29:08 +0530

    Add files via upload

---
+++ maths.py
@@ -0,0 +1,21 @@
+#Add implementation
+def add(x,y):
+	return x+y
+
+#Substract implementation
+def substract(x,y):
+	return x-y	#On Master branch
+
+#Multiply implementation
+def multiply(x,y):
+	return x*y	#On Bug456 branch
+
+#Divide implementation
+def divide(x,y):
+	if y==0		#On Master branch
+		return DIVIDE_BY_ERROR_0
+	else
+		return x/y
+
+def square(x):
+	return x*x
